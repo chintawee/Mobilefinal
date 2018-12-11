0dd9e767af3790b3329ae5c658ca532a901e1116
LAB203_65 <59070023@it.kmitl.ac.th>
2018-12-11 12:33:46 +0700

    go to register

--- app/src/main/java/a59070023/kmitl/ac/th/mobilefinal/LoginFragment.java
+++ app/src/main/java/a59070023/kmitl/ac/th/mobilefinal/LoginFragment.java
@@ -1,7 +1,53 @@
 package a59070023.kmitl.ac.th.mobilefinal;
 
 import android.app.Fragment;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
-public class LoginFragment extends Fragment {
+public class LoginFragment extends android.support.v4.app.Fragment {
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.login_fragment,container,false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+         initRegister();
+    }
+
+    void initRegister()
+    {
+        TextView registerButton = getView().findViewById(R.id.login_register);
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("final", "go to register");
+                getActivity().getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.main_view, new RegisterFragment())
+                        .addToBackStack(null)
+                        .commit();
+            }
+        });
+    }
 }
+
---
+++ app/src/main/java/a59070023/kmitl/ac/th/mobilefinal/RegisterFragment.java
@@ -0,0 +1,35 @@
+package a59070023.kmitl.ac.th.mobilefinal;
+
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class RegisterFragment extends Fragment {
+
+    SQLiteDatabase database;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.register_fragment,container,false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+//        initRegister();
+    }
+
+}

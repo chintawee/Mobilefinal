2dbc4b8b44fe18007cc2398ca68249d4a64b7bca
LAB203_65 <59070023@it.kmitl.ac.th>
2018-12-11 12:46:38 +0700

    if 1

--- app/src/main/java/a59070023/kmitl/ac/th/mobilefinal/RegisterFragment.java
+++ app/src/main/java/a59070023/kmitl/ac/th/mobilefinal/RegisterFragment.java
@@ -5,9 +5,13 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class RegisterFragment extends Fragment {
 
@@ -29,7 +33,31 @@ public class RegisterFragment extends Fragment {
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-//        initRegister();
+        initRegisterNewAccount();
+    }
+
+    void initRegisterNewAccount()
+    {
+        Button registerButton =getView().findViewById(R.id.register_button);
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText userId = getView().findViewById(R.id.register_user);
+                EditText name = getView().findViewById(R.id.register_name);
+                EditText age = getView().findViewById(R.id.register_age);
+                EditText password = getView().findViewById(R.id.register_password);
+                String strUserId = userId.getText().toString();
+                String strName = name.getText().toString();
+                String strAge = age.getText().toString();
+                String strPassword = password.getText().toString();
+
+                if (strUserId.length() < 6 || strUserId.length() > 12)
+                {
+                    Toast.makeText(getActivity(),"ใส่ได้แค่ 6-12 ตัว", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
     }
 
 }
